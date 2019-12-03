959a25ebef2f9259f78ff0f1c41731c54b96f870
lexyaa <55127454+lexyaa@users.noreply.github.com>
2019-12-03 16:12:00 +0900

    13_02_02

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "keywords.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -70,11 +72,13 @@ int main(int argc, char *argv[])
 	//word reading & analysis
 	while(fget_word(fp, word)!=0)
 	{
-		printf("%s\n", word);
+		count_word(word);
 	}
 	
-	fclose(fp);
 	//output
+	print_word();
+	
+	fclose(fp);
 
 	return 0;
 }
